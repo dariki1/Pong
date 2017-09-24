fb8616710b485a8f56c332f60ed55b61170446ab
dariki1 <james_atkins97@hotmail.com>
2017-09-24 16:21:02 +1000

    Reordered code to make more readable, rewrote some to make more dynamic

--- main.js
+++ main.js
@@ -1,23 +1,31 @@
+//TODO; work on speed and collision code
+
+//Constants
+let TAU = Math.PI*2;
+//HTML Elements
 let c = document.getElementById('c');
 let ctx = c.getContext('2d');
-let paddleOne = new Paddle(10);
-let paddleTwo = new Paddle(780);
+//Game Objects
+let paddleOne = new Paddle(1);
+let paddleTwo = new Paddle(97);
 let ball = new Ball();
-let TAU = Math.PI*2;
+//Match variable
+let running = true;
+//Tick variables
 let up = false;
 let down = false;
 
 (function init() {
-	let socket = io();	
+    //Used to communicate with the server
+    let socket = io();	
+    //Add socket listeners
 	socket.on('mPing', function(data) {
 		console.log("Pinged!");
 		socket.emit('mPong');
     });
-    
+    //Initial color/style settings
     document.bgColor = 'black';
 
-    c.width = 800;
-    c.height = 400;
     c.style.left = '0px';
     c.style.top = '0px';
     c.style.position = 'absolute';
@@ -25,6 +33,7 @@ let down = false;
 
     ctx.fillStyle = "#FFFFFF";
 
+    //Event listeners
     window.addEventListener("keydown", function(e) {
         if (e.keyCode == 40) {//Down
             down = true;
@@ -41,88 +50,154 @@ let down = false;
         }
     });
 
+    window.onresize = function() {
+        resize();
+    }
+
+    resize();
+
+    //Initialise game loop
     loop();
 })();
 
-function draw() {
-    ctx.clearRect(0,0,800,400);
-    ctx.fillRect(paddleOne.x,paddleOne.y,10,paddleOne.height);
-    ctx.fillRect(paddleTwo.x,paddleTwo.y,10,paddleTwo.height);
-    ctx.beginPath();
-    ctx.arc(ball.x,ball.y,5,0,TAU);
-    ctx.fill();
-    ctx.stroke();
+function resize() {
+    if (getWidth() / 2 > getHeight()) {
+        c.width = (getHeight()-20)*2;
+        c.height = getHeight()-20;
+    } else {
+        c.width = getWidth()-20;
+        c.height = (getWidth()-20)/2;
+    }
+}
+
+function getWidth() {
+    return window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+}
+
+function getHeight() {
+    return window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+}
+
+function loop() {
+    if (running) {
+        setTimeout(function() {
+            update();
+            draw();
+            if (ball.x < paddleOne.x+2) {
+                alert("P2 wins");
+                running = false;
+            } else if (ball.x > paddleTwo.x) {
+                alert("P1 wins");
+                running = false;
+            }
+            loop();
+        }, 16);
+    }
 }
 
 function update() {
     if (up) {
-        paddleOne.move(-1);
+        paddleOne.move(-0.75);
     }
     if (down) {
-        paddleOne.move(1);
+        paddleOne.move(0.75);
     }
+
+    paddleTwo.y = ball.y-15;
+    //paddleOne.y = ball.y+Math.random()*28-14;
+
     ball.tick();
 }
 
-function loop() {
-    setTimeout(function() {
-        draw();
-        update();
-        loop();
-    }, 16);
-}
-
-function Ball() {
-    this.x = 395;
-    this.y = 195;
-    this.sX = Math.sign(Math.random()-0.5)*Math.random()*3;
-    this.sY = Math.sign(Math.random()-0.5)*Math.random()*3;
-    this.tick = function() {
-        let nX = this.x + this.sX;
-        let nY = this.y + this.sY;
-
-        if (this.x-5 < paddleOne.x+10) {
-            alert("P2 wins");
-        } else if (this.x+5 > paddleTwo.x) {
-            alert("P1 wins");
-        }
-
-        if (nY < 0) {
-            this.sY *= -1;
-            nY = this.y+this.sY;
-        } else if (nY > 400) {
-            this.sY *= -1;
-            nY = this.y+this.sY;
-        }
-
-        if (nX-5 <= paddleOne.x+10 && paddleOne.y <= nY && paddleOne.y+paddleOne.height >= nY) {
-            this.sX *= -1;
-            nX = this.x+this.sX;
-        } else if (nX+5 >= paddleTwo.x && paddleTwo.y <= nY && paddleTwo.y+paddleTwo.height >= nY) {
-            this.sX *= -1;
-            nX = this.X+this.sX;
-        }
-
-        this.x += this.sX;
-        this.y += this.sY;
-        
-    }
+function draw() {
+    ctx.fillStyle = "#FFFFFF";
+    //Clear scene
+    ctx.clearRect(0,0,c.width,c.height);
+    //Draw paddles
+    ctx.fillRect(paddleOne.getPixelX(),paddleOne.getPixelY(),2*c.width/100,paddleOne.getPixelHeight());
+    ctx.fillRect(paddleTwo.getPixelX(),paddleTwo.getPixelY(),2*c.width/100,paddleTwo.getPixelHeight());
+    //Draw ball
+    ctx.beginPath();
+    ctx.arc(ball.getPixelX(),ball.getPixelY(),ball.getPixelRad(),0,TAU);
+    ctx.fill();
 }
 
 function Paddle(nX) {
     this.x = nX;
-    this.y = 170;
-    this.height = 60;
+    this.y = 35;
+    this.height = 30;
+
+    this.getPixelHeight = function() {
+        return this.height*c.height/100;
+    }
+
+    this.getPixelY = function() {
+        return this.y*c.height/100;
+    }
+
+    this.getPixelX = function() {
+        return this.x*c.width/100;
+    }
 
     this.move = function(num) {
         let nY = this.y+num;
 
         if (nY < 0) {
             nY = 0;
-        } else if (nY + this.height > 400) {
-            nY = 400-this.height;
+        } else if (nY + this.height > 100) {
+            nY = 100-this.height;
         }
 
+        this.y = nY;
+    }
+}
+
+function Ball() {
+    this.x = 50;
+    this.y = 50;
+
+    this.sX = Math.random()-0.5;
+    this.sY = Math.random()-0.5;
+
+    this.sX += Math.sign(this.sX)/2;
+    this.sY += Math.sign(this.sY)/2;
+
+    this.radius = 2;
+
+    this.getPixelX = function() {
+        return this.x*c.width/100;
+    }
+
+    this.getPixelY = function() {
+        return this.y*c.height/100;
+    }
+
+    this.getPixelRad = function() {
+        return this.radius*c.height/100;
+    }
+
+    this.tick = function() {
+        let nX = this.x + this.sX;
+        let nY = this.y + this.sY;
+        
+        if (nY < 0) {//Bounce off top
+            this.sY = Math.abs(this.sY);
+            nY = this.y + this.sY;
+        } else if (nY > 100) {//Bounce off bottom
+            this.sY = Math.abs(this.sY)*-1;
+            nY = this.y + this.sY;
+        }
+
+        
+        if (nX <= paddleOne.x+2 && paddleOne.y <= nY && paddleOne.y+paddleOne.height >= nY) {//Bounce off paddle one
+            this.sX = Math.abs(this.sX);
+            nX = this.x + this.sX;
+        } else if (nX >= paddleTwo.x && paddleTwo.y <= nY && paddleTwo.y+paddleTwo.height >= nY) {//Bounce off paddle two
+            this.sX = Math.abs(this.sX)*-1;
+            nX = this.x + this.sX;
+        }
+
+        this.x = nX;
         this.y = nY;
     }
 }
